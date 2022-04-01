236f991ee0a7e9147233be635d7d73cdc917a0b8
Crystal <crystal120599@gmail.com>
2022-04-01 11:08:41 -0700

    user auth routes

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,5 +1,12 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
@@ -38,4 +45,6 @@ router.get(
     return res.json(req.user);
   }
 );
+
+
 module.exports = router;
